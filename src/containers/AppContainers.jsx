c37d454f7dceb11f10571ec495333ad5a3fa06db
ahnan99 <ahnanliu@live.com>
2020-05-02 11:49:05 +0800

    add home layout

--- src/containers/AppContainers.jsx
+++ src/containers/AppContainers.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
-import routes from '../routes'
 import { connect } from 'react-redux'
 import Login from './Login/Login'
 import Register from './Register/Register'
 import ForgetPassword from './ForgetPassword/ForgetPassword'
-
+import MainView from './MainView'
 class App extends Component {
     constructor(props) {
         super(props)
-    }
-
-    get routes() {
-        return (
-            routes.map(route => (
-                <Route key={route.pathKey} exact {...route} />
-            ))
-        )
     }
 
     render() {
@@ -25,7 +16,7 @@ class App extends Component {
                 <Route key={"register"} exact path="/register" component={Register} />
                 <Route key={"resetpassword"} exact path="/forgetpassword" component={ForgetPassword} />
                 <Route key={"login"} exact path="/login" component={Login} />
-                {this.props.application.loggedIn ? this.routes : <Redirect to="/login" />}
+                {this.props.application.loggedIn ? <MainView /> : <Redirect to="/login" />}
             </Switch>
         )
     }
