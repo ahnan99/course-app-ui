6cd8424d325cc74158a97dce594b6daac33880dc
ahnan99 <ahnanliu@live.com>
2020-05-03 10:08:09 +0800

    Test video style

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -2,29 +2,38 @@ import React from 'react';
 import videojs from 'video.js'
 
 export default class VideoPlayer extends React.Component {
-  componentDidMount() {
-    // instantiate Video.js
-    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
-      console.log('onPlayerReady', this)
-    });
-  }
-
-  // destroy player on unmount
-  componentWillUnmount() {
-    if (this.player) {
-      this.player.dispose()
+    componentDidMount() {
+        const config = {
+            autoplay: false,
+            controls: true,
+            sources: [{
+                src: this.props.src,
+                type: 'video/mp4'
+            }]
+        };
+        this.player = videojs(this.videoNode, config, function onPlayerReady() {
+            console.log('onPlayerReady', this);
+        });
     }
-  }
 
-  // wrap the player in a div with a `data-vjs-player` attribute
-  // so videojs won't create additional wrapper in the DOM
-  // see https://github.com/videojs/video.js/pull/3856
-  render() {
-    return (
-     
-        <div data-vjs-player>
-          <video ref={ node => this.videoNode = node } className="video-js vjs-default-skin vjs-big-play-centered" data-setup='{"fluid": true}' controls preload="auto" style={{width:"100%"}}></video>
-        </div>
-    )
-  }
+
+    componentWillUnmount() {
+        if (this.player) {
+            this.player.dispose();
+        }
+    }
+
+    // wrap the player in a div with a `data-vjs-player` attribute
+    // so videojs won't create additional wrapper in the DOM
+    // see https://github.com/videojs/video.js/pull/3856
+    render() {
+        return (
+
+            <div>
+                <div data-vjs-player>
+                    <video ref={node => this.videoNode = node} className="video-js vjs-default-skin video" preload="auto" style={{ width: "100%" }}></video>
+                </div>
+            </div>
+        )
+    }
 }
--- src/containers/ClassPage/ClassPage.jsx
+++ src/containers/ClassPage/ClassPage.jsx
@@ -2,20 +2,12 @@ import React, { Component } from 'react'
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer'
 
 
-const videoJsOptions = {
-    autoplay: false,
-    controls: false,
-    sources: [{
-      src: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
-      type: 'video/mp4'
-    }]
-  }
 export default class ClassPage extends Component {
 
 
     render() {
         return (
-            <VideoPlayer { ...videoJsOptions } />
+            <VideoPlayer src="https://media.w3.org/2010/05/sintel/trailer.mp4" />
         )
     }
 }
