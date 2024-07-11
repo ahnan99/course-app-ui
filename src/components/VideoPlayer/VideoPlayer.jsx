f93545bfaf5d66ffd9da72ca6edfe578328fd8d9
albertliu <xiaochuan_liu@hotmail.com>
2024-07-11 21:15:54 +0800

    video detect msg

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -178,7 +178,7 @@ class VideoPlayer extends Component {
                     footer={[<Button onClick={this.onClickCancel}>取消</Button>, 
                             <Button type='primary' onClick={this.handleClick}>拍照</Button>]} 
                     >
-                    <span style={{'fontSize':'1.3em', 'color':'red'}}>将检测您的正面头像，请平视摄像头{this.state.detectMsg}</span>
+                    <span style={{'fontSize':'1.3em', 'color':'red'}}>将检测您的正面头像，请平视摄像头\n{this.state.detectMsg}</span>
                 </Modal>
                 <Modal
                     visible={false}
